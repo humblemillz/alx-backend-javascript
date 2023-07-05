c76c6a07236ae1dcd4cf5b982881eea9f3ae8757
humblemillz <humblemillions@gmail.com>
2023-07-05 11:03:52 +0000

    task one two and three

--- 0x01-ES6_promise/0x01-ES6_promise/2-then.js
+++
@@ -1,6 +0,0 @@
-export default function handleResponseFromAPI(promise) {
-  return promise
-      .then(() => ({ status: 200, body: 'success' }))
-      .catch(() => new Error()) // If the Promise rejects, return a new error object
-      .finally(() => console.log('Got a response from the API')); // Log a message indicating that a response was received from the API in the final block
-}
